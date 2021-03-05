47c84a2a56436788305239acfd9a9a982640b41a
Jeff Vickers <jeff@unspun.io>
2021-03-04 17:48:22 -0800

    Revert "Update entrypoint.sh"
    This reverts commit c7701e1396c37662804fb0ba787e62595bf66430.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -l
 
-# Testing script for CI.
-
 # echo commands and exit with failure if any commands fail
 set -ex
 
@@ -9,18 +7,5 @@ apt-get update
 
 apt-get install -y \
     clang-format
-
-# Check formatting
-./format/check.sh
-format_ret=$?
-
-# disable echo for summaries
-set +x
-
-# print summaries
-echo format return $format_ret
-
-status=$(format_ret)
-echo exit status $status
-
-exit $status
+    
+./ci.sh
